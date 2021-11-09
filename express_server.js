0bde459fd16f909276224b5b49f3f028992c02a0
dwajsman <danielwajsman@mac.com>
2021-11-09 14:01:24 -0300

    adding url to urlDatabase works

--- express_server.js
+++ express_server.js
@@ -40,6 +40,7 @@ app.get("/urls", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
+  urlDatabase[generateRandomString()] = req.body["longURL"];
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
