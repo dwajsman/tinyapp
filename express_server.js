82dcf4cb4d7cd81d3e61f8eeb9b119473fb6be48
dwajsman <danielwajsman@mac.com>
2021-11-09 12:52:29 -0300

    added views for shrtURL and call on server

--- express_server.js
+++ express_server.js
@@ -8,6 +8,8 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+// const urlDatabase = "/urls";
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -17,8 +19,18 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/hello", (req, res) => {
