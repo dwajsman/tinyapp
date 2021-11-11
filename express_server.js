719cad83a2513bdc7f719923521a8bee46838951
dwajsman <danielwajsman@mac.com>
2021-11-11 00:26:05 -0300

    cookies & login form working -- finally

--- express_server.js
+++ express_server.js
@@ -1,22 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+
 const app = express();
 const PORT = 8080; // default port 8080
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
 
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-const urlDatabase = { 
+
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-// const urlDatabase = "/urls";
 
 
 app.get("/", (req, res) => {
-  // res.send("Hello!");
   res.redirect('/urls');
-
 });
 
 app.listen(PORT, () => {
@@ -31,17 +33,20 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"],
+  };
   res.render("urls_index", templateVars);
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
+  console.log(req.body); // Log the POST request body to the console
   let key = generateRandomString()
   urlDatabase[key] = req.body["longURL"];
   //res.send("Ok");         // Respond with 'Ok' (we will replace this)
@@ -51,26 +56,27 @@ app.post("/urls", (req, res) => {
 app.post("/urls/:shortURL/delete", (req, res) => {
   let key = req.params.shortURL;
   delete urlDatabase[key];
- 
+
   res.redirect('/urls');
 });
 
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: req.params.longURL
+  };
   res.render("urls_show", templateVars);
 
 });
 
 app.post("/urls/:shortURL", (req, res) => {
-  //const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
-  //res.render("urls_show", templateVars);
-  // console.log(req.params.shortURL);
-  // console.log(req.body.newUrl);
-  // console.log(urlDatabase);
   urlDatabase[req.params.shortURL] = req.body.newUrl;
   res.redirect('/urls');
 });
+
+
+
 
 app.get("/u/:shortURL", (req, res) => {
   let key = req.params.shortURL;
@@ -87,11 +93,33 @@ app.get("/u/:shortURL", (req, res) => {
 let generateRandomString = function() {
   let random_string = '';
   let random_ascii;
-  for(let i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
     random_ascii = Math.floor((Math.random() * 25) + 97);
     random_string += String.fromCharCode(random_ascii);
   }
   return random_string;
 };
+
+// cookie
+app.post("/login", (req, res) => {
+  const { username } = req.body;
+
+  res.cookie("username", username);
+  res.cookie("isAuthenticated", true);
+
+  res.redirect('/urls');
+});
+
+
+
+
+app.post("/logout", (req, res) => {
+  const { username } = req.body;
+
+  res.clearCookie('username', username);
+  res.clearCookie('isAuthenticated', null);
+
+  res.redirect('/urls');
+});
 
 
